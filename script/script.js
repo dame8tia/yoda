9eae43b65a4a65263426ffdd4b644cf54eb97b57
dame8tia <laetitia.dwwm48@gmail.com>
2023-06-22 21:01:17 +0200

    Commit final - js OK - Animation OK

--- script/script.js
+++ script/script.js
@@ -3,13 +3,29 @@
 const ck = document.querySelector('#id-newletter');
 const email = document.querySelector('#id-email');
 const btn = document.querySelector('#id-submit');
+const dotTous = document.querySelector('.section-dot');
+const dot = document.querySelectorAll('.dot');
+const citations = document.querySelectorAll('.citation')
+
+
+//dotTous.addEventListener('click', parseDot())
+//function parseDot(){
+//    for (let i = 0; i < dotTous.children.length; i++) {   
+//        dotTous.children[i].addEventListener('click', effetSlide(dotTous.children[i], i)); //effetSlide(dotTous[i], i)
+//        //coucou2(dotTous.children[i]);        
+//    }
+//}
+
+console.log(dot);
+dot.forEach((element)=>{
+    element.addEventListener('click', effetSlide);
+});
 
 ck.addEventListener('click',validation);
 email.addEventListener('click',validation);//'onmouseenter'
 
-
-function validation(){
-
+function validation()
+{
     console.log(ck);
     if (ck.checked){
         console.log("Valeur Email :"+ email.id);
@@ -24,4 +40,27 @@ function validation(){
     else {
         console.log("Case cochée ?" + ck.checked);
     }
+}
+
+function effetSlide(clickDot){ // clickDot est un objet click de l'évément AddEventListener - Possède un attribut target
+
+    console.log("effect", clickDot); // 
+    //console.log(dot, position);
+    let btn = clickDot.target //clickDot.target : origine du click
+    let indexCible =  clickDot.target.dataset.target;// pour récupérer l'attribut data-target qui contient id de la citation à faire apparaitre
+    
+    console.log(citations.length)
+
+    for (let j = 0; j < citations.length; j++) {
+        console.log(indexCible, j);
+        //console.log(citations[j].id, citations[j].hidden);
+        if (indexCible == j){
+            citations[j].hidden = false;
+            console.log("citation N°" + j + " à afficher.")
+        }
+        else {
+            citations[j].hidden = true;
+        }
+      }
+
 }
